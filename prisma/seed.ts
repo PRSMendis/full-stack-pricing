69a75ca60408cb2770935e2213abad1a34c73cc9
PRSMendis <rayhan.mendis@gmail.com>
2024-07-10 15:56:25 +1000

    added more to seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,58 @@
 import { PrismaClient } from '@prisma/client'; // Import the DateTime type
 const prisma = new PrismaClient();
 async function main() {
-  const pinot = await prisma.product.upsert({
+const pinot = await prisma.product.upsert({
     where: { sku: 'HGVPIN216' },
     update: {},
     create: {
-      sku: 'HGVPIN216',
-      name: 'High Garden  Pinot Noir 2021',
-      quantity: 1,
-      brand: 'High Garden ',
-      categoryId: 'Alcoholic Beverage',
-      subCategoryId: 'Wine',
-      segmentId: 'Red',
-      price: 279.06,
-      createdAt: new Date(),
-      updatedAt: new Date(), 
+        sku: 'HGVPIN216',
+        name: 'High Garden  Pinot Noir 2021',
+        quantity: 1,
+        brand: 'High Garden ',
+        categoryId: 'Alcoholic Beverage',
+        subCategoryId: 'Wine',
+        segmentId: 'Red',
+        price: 279.06,
+        createdAt: new Date(),
+        updatedAt: new Date(), 
     },
-  });
-  console.log(pinot)
+});
+
+const brutNature = await prisma.product.upsert({
+    where: { sku: 'KOYBRUNV6' },
+    update: {},
+    create: {
+        sku: 'KOYBRUNV6',
+        name: 'Koyama Methode  Brut Nature NV',
+        quantity: 1,
+        brand: 'Koyama Wines',
+        categoryId: 'Alcoholic Beverage',
+        subCategoryId: 'Wine',
+        segmentId: 'Sparkling',
+        price: 120,
+        createdAt: new Date(),
+        updatedAt: new Date(), 
+    },
+});
+
+const riesling = await prisma.product.upsert({
+    where: { sku: 'KOYNR1837' },
+    update: {},
+    create: {
+        sku: 'KOYNR1837',
+        name: 'Koyama  Riesling 2018',
+        quantity: 1,
+        brand: 'Koyama Wines',
+        categoryId: 'Alcoholic Beverage',
+        subCategoryId: 'Wine',
+        segmentId: 'Port/Dessert',
+        price: 215.04,
+        createdAt: new Date(),
+        updatedAt: new Date(), 
+    },
+});
+
+console.log(pinot, brutNature, riesling);
 }
 main()
   .then(async () => {
