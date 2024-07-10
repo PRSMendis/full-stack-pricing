88a4f137560fa8ad538a2ceeeb73a60d19071ad2
PRSMendis <rayhan.mendis@gmail.com>
2024-07-10 12:04:35 +1000

    fixed up .env.example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+  // const hello = await api.post.hello({ text: "from tRPC" });
+  const hello = await api.product.hello({ text: "from tRPC" });
 
   void api.post.getLatest.prefetch();
 
