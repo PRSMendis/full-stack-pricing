036915ef4e612690586ccf81b1830b8e2cbac0d9
PRSMendis <rayhan.mendis@gmail.com>
2024-07-10 12:24:39 +1000

    added mantine starter code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import "~/styles/globals.css";
+import "@mantine/core/styles.css"
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { MantineProvider, createTheme } from "@mantine/core";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -11,13 +13,21 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const theme = createTheme({
+  /** Put your mantine theme override here */
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <MantineProvider theme={theme}>
+            {children}
+          </MantineProvider>
+        </TRPCReactProvider>
       </body>
     </html>
   );
